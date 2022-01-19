5e9b7c336e050f2a0dcb2d4cfac2a5868cfbe929
ShevelyanchikAlex <shevelyanchik01@mail.ru>
2022-01-19 17:30:17 +0300

    Refactored ServiceFactory.

--- src/main/java/com/epam/admissions/office/service/ServiceFactory.java
+++ src/main/java/com/epam/admissions/office/service/ServiceFactory.java
@@ -1,11 +1,16 @@
 package com.epam.admissions.office.service;
 
-import com.epam.admissions.office.service.impl.UserServiceImpl;
+import com.epam.admissions.office.service.impl.*;
 
 public class ServiceFactory {
     private static final ServiceFactory instance = new ServiceFactory();
 
     private static final UserService userService = new UserServiceImpl();
+    private static final FacultyService facultyService = new FacultyServiceImpl();
+    private static final SubjectService subjectService = new SubjectServiceImpl();
+    private static final ResultService resultService = new ResultServiceImpl();
+    private static final FacultyAdmissionInfoService facultyAdmissionInfoService = new FacultyAdmissionInfoServiceImpl();
+    private static final ApplicationService applicationService = new ApplicationServiceImpl();
 
     private ServiceFactory() {
     }
@@ -17,4 +22,25 @@ public class ServiceFactory {
     public UserService getUserService() {
         return userService;
     }
+
+    public FacultyService getFacultyService() {
+        return facultyService;
+    }
+
+    public SubjectService getSubjectService() {
+        return subjectService;
+    }
+
+    public ResultService getResultService() {
+        return resultService;
+    }
+
+    public FacultyAdmissionInfoService getFacultyAdmissionInfoService() {
+        return facultyAdmissionInfoService;
+    }
+
+    public ApplicationService getApplicationService() {
+        return applicationService;
+    }
+
 }
